b13e34a79c2a50c51f1df786e2fbae8e47e5e474
Tres Serio <treserio@hotmail.com>
2021-06-23 08:12:01 -0700

    After betty looks like poo

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -9,30 +9,37 @@
  */
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+while (1)
+{
+sleep(1);
+}
+return (0);
 }
 /**
  * main - entry point for program
  *
  * Return: always 0
  */
-int main(void) {
-    int cntr, id;
+int main(void)
+{
+int cntr, id;
 
-    for (cntr=0; cntr<5; ++cntr) {
-        id = fork();
-        if (id < 0) {
-            continue;
-        } else if (id == 0) {
-            exit(0);
-        } else {
-            printf("Zombie process created, PID: %d\n", id);
-        }        
-    }
-    infinite_while();
-    return (0);
+for (cntr = 0; cntr < 5; ++cntr)
+{
+id = fork();
+if (id < 0)
+{
+continue;
+}
+else if (id == 0)
+{
+exit(0);
+}
+else
+{
+printf("Zombie process created, PID: %d\n", id);
+}
+}
+infinite_while();
+return (0);
 }
