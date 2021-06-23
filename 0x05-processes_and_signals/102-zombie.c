65ec4cdf208eb8360f0b2fc171e8eb0b830d25e4
Tres Serio <treserio@hotmail.com>
2021-06-22 22:13:40 -0500

    Been awhile since c

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,1 +1,31 @@
-#!/usr/bin/env bash
+#include <stdio.h>
+#include <stdlib.h>
+
+// requirement after 5 zombies are created
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+int main(void) {
+    int cntr, id;
+
+    for (cntr=0; cntr<5; ++cntr) {
+        id = fork();
+        if (id < 0) {
+            continue;
+        }
+        // if parent = 0? seems the thing to do is exit?
+        else if (id == 0) {
+            exit(0);
+        } else {
+            printf("Zombie process created, PID: %s\n" id);
+        }        
+    }
+    infinite_while();
+    return (0);
+}
