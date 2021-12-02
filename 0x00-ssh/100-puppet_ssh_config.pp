1c83c9f670f5259170d70cc9292a10104bf25e82
Tres Serio <treserio@hotmail.com>
2021-12-02 15:38:37 -0600

    100 init, rm keys

---
+++ 0x00-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,8 @@
+ssh_authorized_key { ubuntu@34.73.219.167:
+    user => 'ubuntu',
+    type => 'ssh-rsa',
+    key => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQDRqiZOnXTLKiIDPIvs0VYQj2clcCpGSbamye+rvYMblPC+PLXZ4mGi4/ZNw1oecRqhpk4S2PE24QQ9xEgzDZie8jqw2YsU35gEk7ODOGAzNxSTQRm9godwiOo4tEATdPMGGKhrIxJ4y+8Pk5pCjOLP5ty9G66OLJFTZL8UK6TarQLPWAYdXkFJUpvcatGLBmwtBIGyvRUt3B71MrEmHeAerjdiXpwlaeGDMIOaMK9QnchJ+6OsDVEjFZkvY89CUiuesCCug2JyifZHj7mpKMvrOdOUemoKcVYcpM0D+Y7zyEuZEHsXzFrvJ3A526aCgek2VXL/Qd73fCjCxhJ1AogyjGihbNhjeNr8ZFWarUHsvDdwii/WCKmcGO57kjkOoBidCxlPbY2SY2stRhvDXy58BWUeAW97Mrhi4ZYB1imMwIbhcgu0D95eX+xtIkb6wyR10J6fTN4zBowoprFDePLAmsa1egDNBLd0x5/hD0HmT2MH88NeNTCwIBoEtCpZQCvwR0ingR9ocNRIbyJJiyKKexRHpBQL9ePMAC5UFCS1yx3OiU2V9UFOoDep0gs3PeQbqqwbMrOU0+xdD8nIL7iVzXD5lr5JDyFrsGQ+QfLWSFbiUiQKJRbECBM5GIe+yqU6QGRmILNcOlWR33ensyC4WYst4p0DGj66RInrCcnODw==',
+    options => [
+        'PasswordAuthentication' => 'no'
+    ]
+}
