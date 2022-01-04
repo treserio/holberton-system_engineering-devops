497e0fef3c1b3d6929e419286d69218a181033d8
Tres Serio <treserio@hotmail.com>
2022-01-04 16:08:51 -0600

    0x0E init

---
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        usr = requests.get(
+            f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}").json()
+        todos = requests.get(
+            f"https://jsonplaceholder.typicode.com/todos/?userId={sys.argv[1]}").json()
+
+        print(f'Employee {usr.get("name")} is done with tasks('
+              f'{len([x for x in todos if x.get("completed") == True])}/{len(todos)}):'
+              )
+        print('\n'.join(f'\t {x.get("title")}'
+              for x in todos if x.get('completed') == True))
