8148d6c3fcbc34b599cfd47cfd382141eba8f8b3
Tres Serio <treserio@hotmail.com>
2022-06-08 00:56:26 -0500

    100.1 added + to from field.../sigh

--- 0x03-regular_expressions/100-textme.rb
+++ 0x03-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:([\w\d]+)\] \[to:([+\w\d]+)\] \[flags:([\d:-]+)\]/).join(',')
+puts ARGV[0].scan(/\[from:([+\w\d]+)\] \[to:([+\w\d]+)\] \[flags:([\d:-]+)\]/).join(',')
